4f5a4cb3f96b25019947d089a22ed107e12e4d9a
Ben Selfridge <benselfridge@galois.com>
2025-02-20 12:47:51 -0800

    Working on section 2.4

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,86 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  apply le_antisymm
+  . show a ⊔ b ≤ b ⊔ a
+    apply max_le
+    . show a ≤ b ⊔ a
+      apply le_max_right
+    . show b ≤ b ⊔ a
+      apply le_max_left
+  . show b ⊔ a ≤ a ⊔ b
+    apply max_le
+    . show b ≤ a ⊔ b
+      apply le_max_right
+    . show a ≤ a ⊔ b
+      apply le_max_left
+
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
-theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  . show a ⊓ b ⊓ c ≤ a ⊓ (b ⊓ c)
+    apply le_min
+    . show a ⊓ b ⊓ c ≤ a
+      calc a ⊓ b ⊓ c ≤ a ⊓ b := by apply min_le_left
+           _         ≤ a     := by apply min_le_left
+    . show a ⊓ b ⊓ c ≤ b ⊓ c
+      apply le_min
+      . show a ⊓ b ⊓ c ≤ b
+        calc a ⊓ b ⊓ c ≤ a ⊓ b := by apply min_le_left
+           _         ≤ b       := by apply min_le_right
+      . show a ⊓ b ⊓ c ≤ c
+        apply min_le_right
+  . show a ⊓ (b ⊓ c) ≤ a ⊓ b ⊓ c
+    apply le_min
+    . show a ⊓ (b ⊓ c) ≤ a ⊓ b
+      apply le_min
+      . show a ⊓ (b ⊓ c) ≤ a
+        apply min_le_left
+      . show a ⊓ (b ⊓ c) ≤ b
+        calc a ⊓ (b ⊓ c) ≤ b ⊓ c := by apply min_le_right
+             _           ≤ b     := by apply min_le_left
+    . show a ⊓ (b ⊓ c) ≤ c
+      calc a ⊓ (b ⊓ c) ≤ b ⊓ c := by apply min_le_right
+           _           ≤ c     := by apply min_le_right
+
+theorem aux : a ⊓ b + c ≤ (a + c) ⊓ (b + c) := by
+  apply le_min
+  . show a ⊓ b + c ≤ a + c
+    apply add_le_add_right; apply min_le_left
+  . show a ⊓ b + c ≤ b + c
+    apply add_le_add_right; apply min_le_right
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  . show a ⊓ b + c ≤ (a + c) ⊓ (b + c)
+    apply aux
+  . show (a + c) ⊓ (b + c) ≤ a ⊓ b + c
+    have h₁ := aux (a + c) (b + c) (-c)
+    rw [← neg_add_cancel_right ((a + c) ⊓ (b + c)) c]
+    show (a + c) ⊓ (b + c) + (-c) + c ≤ a ⊓ b + c
+    apply add_le_add_right
+    repeat (rw [add_assoc, add_neg_cancel, add_zero] at h₁)
+    exact h₁
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+#check add_sub_cancel_right
+
+example (h : a ≤ c + b) : a - b ≤ c := by
+  apply?
+
+example (h : a ≤ b) : a - c ≤ b - c := by apply?
+
+#check (sub_le_sub_right : a ≤ b → (c : ℝ) → a - c ≤ b - c)
+
+#check sub_add_cancel
+#check sub_le_sub
+
+example : |a| - |b| ≤ |a - b| := by
+  calc |a| - |b| = |a - b + b| - |b| := by rw [sub_add_cancel]
+      _         ≤ |a - b| + |b| - |b| := by apply sub_le_sub_right; apply abs_add
+
+
 end
 
 section
@@ -80,5 +149,3 @@ variable (m n : ℕ)
 example : Nat.gcd m n = Nat.gcd n m := by
   sorry
 end
-
-
